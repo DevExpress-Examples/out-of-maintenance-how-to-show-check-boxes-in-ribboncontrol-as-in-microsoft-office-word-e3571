802c28fed90dae633790b8c5ba145f3cd4dbbf87
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/MyRibbonControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.Utils;
+using DevExpress.Skins;
+
+namespace DXSample
+{
+    public class MyRibbonControl : RibbonControl
+    {
+        public MyRibbonControl() : base() {
+            DevExpress.LookAndFeel.UserLookAndFeel.Default.StyleChanged += OnStyleChanged;
+        }
+
+        void OnStyleChanged(object sender, EventArgs e)
+        {
+            UpdateCheckItemGlyph();
+        }
+
+        protected override RibbonBarManager CreateBarManager()
+        {
+            return new MyRibbonBarManager(this);
+        }
+
+        internal void UpdateCheckItemGlyph()
+        {
+            Manager.CheckImage = null;
+            Manager.UncheckImage = null;
+        }
+
+        public new MyRibbonBarManager Manager {
+            get { return base.Manager as MyRibbonBarManager; } 
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing)
+                DevExpress.LookAndFeel.UserLookAndFeel.Default.StyleChanged -= OnStyleChanged;
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VB/WindowsFormsApplication1/Main.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+
+
+Namespace DXSample
+	Partial Public Class Main
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+			propertyGridControl1.SelectedObject = defaultLookAndFeel1.LookAndFeel
+		End Sub
+	End Class
+End Namespace
